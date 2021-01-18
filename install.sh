d93dd5c2893f26e180f35bc586ec5226026f63f6
ALEN TL <alentl@pop-os.localdomain>
2021-01-18 08:47:33 +0530

    First commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gcc -o kbdusb kbdusb.c &&\
+sudo cp kbdusb /usr/local/bin/. &&\
+sudo cp hp_keyboard.sh /usr/local/bin/. &&\
+sudo cp hp_keyboard.service /etc/systemd/system/. &&\
+sudo systemctl enable hp_keyboard.service &&\
+sudo systemctl start hp_keyboard.service
